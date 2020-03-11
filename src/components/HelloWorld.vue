2bb311d6b940b92e789dbb971e74c3ebdfd6ed58
Thor Anderson <ta.anderson@gmail.com>
2020-03-11 15:29:46 -0600

    added search capability, truncation, etc.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,64 @@
 <template>
   <!-- Apollo watched Graphql query -->
-    <ApolloQuery
-      :query="require('../graphql/HelloWorld.gql')"
-    >
-      <template v-slot="{ result: { loading, error, data } }">
-        <!-- Loading -->
-        <div v-if="loading" class="loading apollo">Loading...</div>
+  <ApolloQuery :query="require('../graphql/HelloWorld.gql')"
+   :variables="{ searchString }">
+    <template v-slot="{ result: { loading, error, data } }">
+      <!-- Loading -->
+      <div v-if="loading" class="loading apollo">Loading...</div>
 
-        <!-- Error -->
-        <div v-else-if="error" class="error apollo">An error occured</div>
+      <!-- Error -->
+      <div v-else-if="error" class="error apollo">An error occured</div>
 
-        <!-- Result -->
-        <div v-else-if="data" class="result apollo">{{ data.Courses[0].name }}
-          <v-list>
-            <v-list-item v-for="(item, i) in data.Courses" :key="i">
-              <p> {{ item.name }} </p>
-            </v-list-item>
-          </v-list>
-        </div>
+      <!-- Result -->
+      <div v-else-if="data" class="result apollo">
+        <v-row>
+          <v-col cols="4" v-for="(item, i) in data.Courses" :key="i">
+            <v-card class="mx-auto" max-width="350">
+              <v-card-text>
+                <div>{{ item.courseCode }}</div>
+                <p class="courseName">
+                  {{ item.name }}
+                </p>
+                <p>{{ item.termsOffered }}</p>
+                <div class="text--primary">
+                  {{ item.description | truncate(200) }}
+                </div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn text color="deep-purple accent-4">
+                  Learn More
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
 
-        <!-- No result -->
-        <div v-else class="no-result apollo">No result :(</div>
-      </template>
-    </ApolloQuery>
+      <!-- No result -->
+      <div v-else class="no-result apollo">No result :(</div>
+    </template>
+  </ApolloQuery>
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
+//import vue-truncate-filter from 'vue-truncate-filter'
 
-    data: () => ({
-      
-    }),
-  }
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({
+    searchString: "DGM"
+  }),
+}
 </script>
+
+<style scoped>
+.result {
+  padding: 1rem;
+}
+
+.courseName {
+  font-size: 1.25rem;
+  font-weight: 600;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
 import { createProvider } from './vue-apollo'
+import VueTruncate from 'vue-truncate-filter'
+
+Vue.use(VueTruncate)
 
 Vue.config.productionTip = false
 
