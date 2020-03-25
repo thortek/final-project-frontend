62dc46b3b7449f9721869a2dbf0db3489166da24
Thor Anderson <ta.anderson@gmail.com>
2020-03-25 16:36:03 -0600

    Adding form items

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -11,22 +11,76 @@
     @done="onDone"
   >
     <template v-slot="{ mutate, loading, error }">
-      <button :disabled="loading" @click="mutate()">Click me</button>
+      <v-form>
+    <v-container >
+      <v-row>
+        <v-col
+          cols="12"
+          md="12"
+        >
+          <v-text-field
+            v-model="name"
+            label="Course name"
+            required filled
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          cols="12"
+          md="12"
+        >
+          <v-textarea filled clearable rows="4"
+            v-model="description"
+            label="Course description"
+          ></v-textarea>
+        </v-col>
+      </v-row>
+      
+      <v-row>
+        <v-col cols="12" md="5">
+          <v-select
+          v-model="creditItems"
+          filled
+          label="Default credits"
+        ></v-select>
+        </v-col>
+        <v-col cols="12" md="2">
+          <v-text-field
+          v-model="courseCode"
+          filled
+          label="Couse code"
+        ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="5">
+          <v-select
+          v-model="terms"
+          filled
+          label="Terms offered"
+        ></v-select>
+        </v-col>
+      </v-row>
+      <v-btn large color="primary" :disabled="loading" @click="mutate()">Save</v-btn>
       <p v-if="error">An error occurred: {{ error }}</p>
+    </v-container>
+  </v-form>
+      
     </template>
   </ApolloMutation>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      name: 'adfd',
-      description: 'adfa',
-      defaultCredits: 'adf',
+  data: function () {
+      return { 
+      name: '',
+      description: '',
+      defaultCredits: '',
       courseCode: 'DGM 9000',
-      termsOffered: 'adf',
-    }
+      termsOffered: '',
+      creditItems: ['1', '2', '3', '4'],
+      terms: ['Fall', 'Spring', 'Summer', 'Fall/Spring', 'Spring/Summer', 'Fall/Spring/Summer']
+      }
   },
   methods: {
     onDone() {
